82e16e50bfe046c06405882d4c0b13333f56c5d9
leixingyu <techartlei@gmail.com>
2022-09-15 20:12:10 -0400

    add text edit shortcut

--- codeEditor/codeEditor.py
+++ codeEditor/codeEditor.py
@@ -17,8 +17,152 @@ class LineNumberArea(QtWidgets.QWidget):
     def paintEvent(self, event):
         self._code_editor.lineNumberAreaPaintEvent(event)
 
+class CodeTextEdit(QtWidgets.QPlainTextEdit):
+    is_first = False
+    pressed_keys = list()
 
-class CodeEditor(QtWidgets.QPlainTextEdit):
+    indented = QtCore.Signal(object)
+    unindented = QtCore.Signal(object)
+    commented = QtCore.Signal(object)
+    uncommented = QtCore.Signal(object)
+
+    def __init__(self):
+        super(CodeTextEdit, self).__init__()
+
+        self.indented.connect(self.do_indent)
+        self.unindented.connect(self.undo_indent)
+        self.commented.connect(self.do_comment)
+        self.uncommented.connect(self.undo_comment)
+
+    def clear_selection(self):
+        """
+        Clear text selection on cursor
+        """
+        pos = self.textCursor().selectionEnd()
+        self.textCursor().movePosition(pos)
+
+    def get_selection_range(self):
+        """
+        Get text selection line range from cursor
+        Note: currently only support continuous selection
+        :return: (int, int). start line number and end line number
+        """
+        cursor = self.textCursor()
+        if not cursor.hasSelection():
+            return 0, 0
+
+        start_pos = cursor.selectionStart()
+        end_pos = cursor.selectionEnd()
+
+        cursor.setPosition(start_pos)
+        start_line = cursor.blockNumber()
+        cursor.setPosition(end_pos)
+        end_line = cursor.blockNumber()
+
+        return start_line, end_line
+
+    def remove_line_start(self, string, line_number):
+        """
+        Remove certain string occurrence on line start
+        :param string: str. string pattern to remove
+        :param line_number: int. line number
+        """
+        cursor = QtGui.QTextCursor(
+            self.document().findBlockByLineNumber(line_number))
+        cursor.select(QtGui.QTextCursor.LineUnderCursor)
+        text = cursor.selectedText()
+        if text.startswith(string):
+            cursor.removeSelectedText()
+            cursor.insertText(text.split(string, 1)[-1])
+
+    def insert_line_start(self, string, line_number):
+        """
+        Insert certain string pattern on line start
+        :param string: str. string pattern to insert
+        :param line_number: int. line number
+        """
+        cursor = QtGui.QTextCursor(
+            self.document().findBlockByLineNumber(line_number))
+        self.setTextCursor(cursor)
+        self.textCursor().insertText(string)
+
+    def keyPressEvent(self, event):
+        """
+        Extend the key press event to create key shortcuts
+        """
+        self.is_first = True
+        self.pressed_keys.append(event.key())
+        start_line, end_line = self.get_selection_range()
+
+        # indent event
+        if event.key() == QtCore.Qt.Key_Tab and \
+                (end_line - start_line):
+            lines = range(start_line, end_line+1)
+            self.indented.emit(lines)
+            return
+
+        # un-indent event
+        elif event.key() == QtCore.Qt.Key_Backtab:
+            lines = range(start_line, end_line+1)
+            self.unindented.emit(lines)
+            return
+
+        super(CodeTextEdit, self).keyPressEvent(event)
+
+    def keyReleaseEvent(self, event):
+        """
+        Extend the key release event to catch key combos
+        """
+        if self.is_first:
+            self.process_multi_keys(self.pressed_keys)
+
+        self.is_first = False
+        self.pressed_keys.pop()
+        super(CodeTextEdit, self).keyReleaseEvent(event)
+
+    def process_multi_keys(self, keys):
+        """
+        Placeholder for processing multiple key combo events
+        :param keys: [QtCore.Qt.Key]. key combos
+        """
+        # toggle comments indent event
+        if keys == [QtCore.Qt.Key_Control, QtCore.Qt.Key_Slash]:
+            pass
+
+    def do_indent(self, lines):
+        """
+        Indent lines
+        :param lines: [int]. line numbers
+        """
+        for line in lines:
+            self.insert_line_start('\t', line)
+
+    def undo_indent(self, lines):
+        """
+        Un-indent lines
+        :param lines: [int]. line numbers
+        """
+        for line in lines:
+            self.remove_line_start('\t', line)
+
+    def do_comment(self, lines):
+        """
+        Comment out lines
+        :param lines: [int]. line numbers
+        """
+        for line in lines:
+            pass
+
+    def undo_comment(self, lines):
+        """
+        Un-comment lines
+        :param lines: [int]. line numbers
+        """
+        for line in lines:
+            pass
+
+
+class CodeEditor(CodeTextEdit):
     def __init__(self):
         super(CodeEditor, self).__init__()
         self.setLineWrapMode(QtWidgets.QPlainTextEdit.NoWrap)
